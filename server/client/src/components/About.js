faf57a63930e51de3f338557849e7c898357863a
ofek <ezra@gmail.com>
2020-04-02 10:31:32 +0300

    changes

--- server/client/src/components/About.js
+++ server/client/src/components/About.js
@@ -4,7 +4,39 @@ import "./About.css";
 const About = () => {
   return (
     <div className="About">
-      <div style={{ width: "70%" }}>
+      <div className="About-img">
+        <img src="http://via.placeholder.com/640x360" alt="img"/>
+      </div>
+
+      <div className="About-description">
+        <h1 className="About-description-title">Nathan Krasney</h1>
+        <h4 className="About-description-subtitle">
+          CTO , Developer , Lecturer and Mentor
+        </h4>
+
+        <p className="About-description-details">
+          I am doing software development for almost 25 years.
+          I have my own business for software development and teaching
+          courses for the last 8 years.
+          I have led 3 successful web full-stack bootcamps.
+          I have 3 online courses on udemy using my unique way of teaching.
+          All of this gives me the experience and capabilities required to
+          mentor junior web developer in the way to first programming job
+        </p>
+
+        <div className="About-description-images">
+          <div className="About-description-img-1 img">
+            <img src="http://via.placeholder.com/300x300" alt="img"/>
+          </div>
+          <div className="About-description-img-1 img">
+            <img src="http://via.placeholder.com/300x300" alt="img"/>
+          </div>
+          <div className="About-description-img-1 img">
+            <img src="http://via.placeholder.com/300x300" alt="img"/>
+          </div>
+        </div>
+      </div>
+      {/* <div style={{ width: "70%" }}>
         <p style={{ color: "red" }}>
           sometimes it take 0.5 sec for the image to show up , should it be
           imported as done{" "}
@@ -25,7 +57,7 @@ const About = () => {
           All of this gives me the experience and capabilities required to
           mentor junior web developer in the way to first programming job
         </p>
-      </div>
+      </div> */}
     </div>
   );
 };

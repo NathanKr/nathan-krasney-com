9c7acf1736e8192e420b877cb20fb05d1f8c628f
Nathan Krasney <natankrasney@gmail.com>
2020-04-06 16:21:53 +0300

    production ready for digital ocean

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 console.log("app is loading");
 const express = require("express");
 const app = express();
+const utils = require('./production_utils')
+
 
 // used for json inside body 
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-  console.log("root is accessed");
-  res.send({res:"result from server"});
-});
 
-const PORT = process.env.PORT || 5000;
+// --- must be last before listen because it handle non served route
+utils.handleProduction(express,app);
+
+const PORT = 5000; 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });

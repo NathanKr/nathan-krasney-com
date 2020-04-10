39b09517d529a41d8efb99ccad4ae6d6429a81bc
ofek <ezra@gmail.com>
2020-04-10 12:55:18 +0300

    fixing navbar

--- server/client/src/components/Top.js
+++ server/client/src/components/Top.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./Home";
 import Contact from "./Contact";
 import Courses from "./Courses";
@@ -12,27 +12,45 @@ import "./Top.css";
 import { Navbar, Nav } from "react-bootstrap";
 
 const Top = () => {
+  const [navExpended, setNavExpended] = useState(false);
+
   return (
     <div className="Top">
       <header className="header">
         {
-          <Navbar collapseOnSelect expand="xl"  variant="dark">
+          <Navbar expanded={navExpended} collapseOnSelect expand="xl" variant="dark">
             <Navbar.Brand>
               <h2>
                 <span className="nameColor">Nathan</span> Krasney
               </h2>
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" 
+            onClick={() => setNavExpended(navExpended ? false : "expanded")}
+            />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto"></Nav>
               <Nav>
-                <Link className="navigationLink" to="/">Home</Link>
-                <Link className="navigationLink" to="/About">About</Link>
-                <Link className="navigationLink" to="/Contact">Contact</Link>
-                <Link className="navigationLink" to="/Courses">Courses</Link>
-                <Link className="navigationLink" to="/Mentoring">Mentoring</Link>
-                <Link className="navigationLink" to="/Testimonials">Testimonials</Link>
-                <Link className="navigationLink" to="/Events">Events</Link>
+                <Link onClick={() =>setNavExpended(false)} className="navigationLink" to="/">
+                  Home
+                </Link>
+                <Link onClick={() =>setNavExpended(false)} className="navigationLink" to="/About">
+                  About
+                </Link>
+                <Link onClick={() =>setNavExpended(false)} className="navigationLink" to="/Contact">
+                  Contact
+                </Link>
+                <Link onClick={() =>setNavExpended(false)} className="navigationLink" to="/Courses">
+                  Courses
+                </Link>
+                <Link onClick={() =>setNavExpended(false)} className="navigationLink" to="/Mentoring">
+                  Mentoring
+                </Link>
+                <Link onClick={() =>setNavExpended(false)} className="navigationLink" to="/Testimonials">
+                  Testimonials
+                </Link>
+                <Link onClick={() =>setNavExpended(false)} className="navigationLink" to="/Events">
+                  Events
+                </Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>

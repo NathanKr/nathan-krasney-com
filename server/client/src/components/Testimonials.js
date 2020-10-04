cde64d5296eee09e71d0edd28fd58ef2ff063c84
Nathan Krasney <natankrasney@gmail.com>
2020-10-04 09:31:08 +0300

    fix testemonials images

--- server/client/src/components/Testimonials.js
+++ server/client/src/components/Testimonials.js
@@ -13,7 +13,7 @@ const Testimonials = () => {
         ' "Hi Nathan , How are you ? As you know i have signed a working contract and i am allready working for two weeks. Thanks to you i have passed these weeks without the fears i had at the begining. I want to thank you for the support and tools that you have gave us throughout the bootcamp. The way that you have thought me was very helpfull. I have no doubt that you are an excellent lecturer "',
       href: "https://www.linkedin.com/in/mazal-gevyan/",
       img_src:
-        "https://media-exp1.licdn.com/dms/image/C5603AQHu-tEHAQMLhQ/profile-displayphoto-shrink_200_200/0?e=1591228800&v=beta&t=NJzGe0pbFxyZ1eA7o1-ILa8spQPyXo_DkppwP8PoVmQ",
+        "https://media-exp1.licdn.com/dms/image/C5603AQHu-tEHAQMLhQ/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=WwT90wsKPhNatepSjihkooo-VeN5JtqUihMcpWbmpoA",
     },
     {
       testimonial_category: testimonial_category_bootcamp,
@@ -23,7 +23,7 @@ const Testimonials = () => {
         ' "Nathan, it is all thanks to you. You are an amazing teacher !!! After one and a half month on the new job i can say that you have realy gave me all the tools that i need to cope with any challange. Huge thanks !!!" ',
       href: "https://www.linkedin.com/in/aliza-zeru-5a2876149/",
       img_src:
-        "https://media-exp1.licdn.com/dms/image/C4D03AQF7WGqVFUEVAg/profile-displayphoto-shrink_200_200/0?e=1591228800&v=beta&t=9FLY-Pb7T6j4t5Ab1QND1n9VV99ViDGoEt5M2XBNXXA",
+        "https://media-exp1.licdn.com/dms/image/C4D03AQF7WGqVFUEVAg/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=cUUJyp42lxLfbymziKVf0yCasN0UEcHSeY2-1ISh_P4",
     },
     {
       testimonial_category: testimonial_category_bootcamp,
@@ -33,7 +33,7 @@ const Testimonials = () => {
         ' "Hi Nathan, i want to inform that i got a job, today was my first day. Thanks to all the tools that we have acquired from you during the bootcamp my integration into the job was relatively smooth. Thank you very much ! I could have not asked for a better lecturer than you " ',
       href: "https://www.linkedin.com/in/batchen-itbarach/",
       img_src:
-        "https://media-exp1.licdn.com/dms/image/C4D03AQHvBVOOhThpcQ/profile-displayphoto-shrink_200_200/0?e=1591228800&v=beta&t=-hurJGIKDBj_8Y3_F8u6nd-2MPS8pY_uBL_r1rvRqtk",
+        "https://media-exp1.licdn.com/dms/image/C4D03AQHvBVOOhThpcQ/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=l9pHqxMKCjYOw6yEzYnm07Uio9vkda37Y3I_P1G8rFM",
     },
     {
       testimonial_category: testimonial_category_bootcamp,
@@ -44,7 +44,7 @@ const Testimonials = () => {
       href:
         "https://www.linkedin.com/in/chaim-ochs-3089b563/?originalSubdomain=il",
       img_src:
-        "https://media-exp1.licdn.com/dms/image/C5603AQHqoPo8w8DpTA/profile-displayphoto-shrink_800_800/0?e=1591833600&v=beta&t=NhYTcEqhgsSOY4YEu21zikyoO5-GCqclKqqdkqKnjjM",
+        "https://media-exp1.licdn.com/dms/image/C5603AQHqoPo8w8DpTA/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=st1_tNWTM9jmfRJitDBBdAUHnaE5z-mi-kxUoM7W2Zk",
     },
     {
       testimonial_category: testimonial_category_bootcamp,
@@ -54,7 +54,7 @@ const Testimonials = () => {
         "'Very great course. I've learned React in a really fast way. Exercises after each lesson gives a lot. Response from a teacher is really quick. :)' ",
       href: "https://www.linkedin.com/in/%C5%82ukasz-franczyk-b76333146/",
       img_src:
-        "https://media-exp1.licdn.com/dms/image/C5103AQE9gi3XrebUmw/profile-displayphoto-shrink_800_800/0?e=1591833600&v=beta&t=15nimASkr9T6n4prWTVum9_9mYS9rgtlyCbrkiSNs50",
+        "https://media-exp1.licdn.com/dms/image/C5103AQE9gi3XrebUmw/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=V87JdAcT1hsSlnfADIf25oZkexzaZnMlplO_JBQI2LU",
     },
   ];
 
